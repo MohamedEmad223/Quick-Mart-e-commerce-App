a9394ea7f00a2a4f8dd43244c444128a36636ad4
mohamed emad <mohamedelbakrawy70@gmail.com>
2024-09-04 15:17:01 +0300

    - add the text of onboarding into app text file

---
+++ lib/core/helpers/app_text.dart
@@ -0,0 +1,13 @@
+class AppText {
+  //----------------- OnBoarding Screen -----------------
+  static const String onBoardingtextOne = 'Explore a wide range of products';
+  static const String onBoardingtextTwo =
+      'Unlock exclusive offers and discounts';
+  static const String onBoardingtextThree = 'Safe and secure payments';
+  static const String onBoardingDescriptionOne =
+      'Explore a wide range of products at your fingertips. QuickMart offers an extensive collection to suit your needs.';
+  static const String onBoardingDescriptionTwo =
+      'Get access to limited-time deals and special promotions available only to our valued customers.';
+  static const String onBoardingDescriptionThree =
+      'QuickMart employs industry-leading encryption and trusted payment gateways to safeguard your financial information.';
+}
