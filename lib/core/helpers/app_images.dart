c1e29069f517b4b42996b84d2fc566699373a2ba
mohamed emad <mohamedelbakrawy70@gmail.com>
2024-09-04 15:09:27 +0300

    - add on boarding images for app_image file

--- lib/core/helpers/app_images.dart
+++ lib/core/helpers/app_images.dart
@@ -1,4 +1,11 @@
 class AppImages {
   //----------------splash screen----------------
   static const String splashLogo = 'assets/svgs/splash/quickmart.svg';
+  //----------------onboarding screen----------------
+  static const String onboarding1 =
+      'assets/images/on_boarding/on_boarding_one.png';
+  static const String onboarding2 =
+      'assets/images/on_boarding/on_boarding_two.png';
+  static const String onboarding3 =
+      'assets/images/on_boarding/on_boarding_three.png';
 }
