9b797ede912a0cea31187a346613cbdf9e629b75
mohamed emad <mohamedelbakrawy70@gmail.com>
2024-09-04 15:37:22 +0300

    - fix fonts error in screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:quick_mart/core/routes/app_routes.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:quick_mart/quick_mart.dart';
 
 void main() {
-  runApp(QuickMart(
-    appRoutes: AppRoutes(),
-  ));
+  GoogleFonts.config.allowRuntimeFetching = false;
+  LicenseRegistry.addLicense(
+    () async* {
+      final license = await rootBundle.loadString('assets/fonts/OFL.txt');
+      yield LicenseEntryWithLineBreaks(
+        ['assets/fonts/PlusJakartaSans'],
+        license,
+      );
+    },
+  );
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(const QuickMart());
+  });
+  runApp(const QuickMart());
 }
--- lib/quick_mart.dart
+++ lib/quick_mart.dart
@@ -4,9 +4,7 @@ import 'package:quick_mart/core/routes/app_routes.dart';
 import 'package:quick_mart/core/routes/routes.dart';
 
 class QuickMart extends StatelessWidget {
-  const QuickMart({super.key, required this.appRoutes});
-
-  final AppRoutes appRoutes;
+  const QuickMart({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +15,7 @@ class QuickMart extends StatelessWidget {
       builder: (context, _) => MaterialApp(
         debugShowCheckedModeBanner: false,
         initialRoute: Routes.splash,
-        onGenerateRoute: appRoutes.onGenerateRoute,
+        onGenerateRoute: AppRoutes().onGenerateRoute,
       ),
     );
   }
