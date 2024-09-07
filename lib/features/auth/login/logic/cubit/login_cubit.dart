352cac0e1ab2fd14cff2401a85e75cda703f4353
mohamed emad <mohamedelbakrawy70@gmail.com>
2024-09-07 17:41:08 +0300

    - finish login cubit

--- lib/features/auth/login/logic/cubit/login_cubit.dart
+++ lib/features/auth/login/logic/cubit/login_cubit.dart
@@ -1,10 +1,27 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 import '../../data/model/login_model.dart';
+import '../../data/repo/login_repo.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginInitial());
+  LoginCubit(this._loginRepo) : super(LoginInitial());
+
+  late LoginRepo _loginRepo;
+
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+
+  Future<void> login(String path, String data) async {
+    emit(LoginLoading());
+    final result = await _loginRepo.login(path, data);
+    result.fold(
+      (error) => emit(LoginError(message: error)),
+      (success) => emit(LoginSuccess(loginModel: success)),
+    );
+  }
+
 }
