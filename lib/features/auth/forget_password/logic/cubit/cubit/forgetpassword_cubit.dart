72266a8547d9460695ebb806f35a286168eb2ef6
mohamed emad <mohamedelbakrawy70@gmail.com>
2024-09-28 12:14:33 +0300

    finish forget password states

--- lib/features/auth/forget_password/logic/cubit/cubit/forgetpassword_cubit.dart
+++ lib/features/auth/forget_password/logic/cubit/cubit/forgetpassword_cubit.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
+import '../../../data/model/forget_password_model.dart';
+
 part 'forgetpassword_state.dart';
 
 class ForgetpasswordCubit extends Cubit<ForgetpasswordState> {
--- lib/features/auth/forget_password/logic/cubit/cubit/forgetpassword_state.dart
+++ lib/features/auth/forget_password/logic/cubit/cubit/forgetpassword_state.dart
@@ -4,3 +4,17 @@ part of 'forgetpassword_cubit.dart';
 sealed class ForgetpasswordState {}
 
 final class ForgetpasswordInitial extends ForgetpasswordState {}
+
+final class ForgetpasswordLoading extends ForgetpasswordState {}
+
+final class ForgetpasswordSuccess extends ForgetpasswordState {
+  final ForgetPasswordModel? forgetPasswordModel;
+
+  ForgetpasswordSuccess({this.forgetPasswordModel});
+}
+
+final class ForgetpasswordError extends ForgetpasswordState {
+  final String? message;
+
+  ForgetpasswordError({this.message});
+}
