60e74e87a87e325cfe9187a8fb2d42671900c9ba
mohamed emad <mohamedelbakrawy70@gmail.com>
2024-09-28 13:11:40 +0300

    finish VerifycodeCubit

--- lib/features/auth/forget_password/logic/code_verfication/cubit/verifycode_cubit.dart
+++ lib/features/auth/forget_password/logic/code_verfication/cubit/verifycode_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quick_mart/features/auth/forget_password/data/repo/verify_code_repo.dart';
 
 import '../../../data/model/verify_code_model.dart';
 
 part 'verifycode_state.dart';
 
 class VerifycodeCubit extends Cubit<VerifycodeState> {
-  VerifycodeCubit() : super(VerifycodeInitial());
+  VerifycodeCubit(this._verifyCodeRepo) : super(VerifycodeInitial());
+
+  final VerifyCodeRepo _verifyCodeRepo;
+
+  Future<void> verifyCode(String path, Map<String, dynamic> data) async {
+    emit(VerifycodeLoading());
+    final result = await _verifyCodeRepo.verifyCode(path, data);
+    result.fold(
+      (error) => emit(VerifycodeError(message: error)),
+      (success) => emit(VerifycodeSuccess(verfiyResetCodeModel: success)),
+    );
+  }
 }
