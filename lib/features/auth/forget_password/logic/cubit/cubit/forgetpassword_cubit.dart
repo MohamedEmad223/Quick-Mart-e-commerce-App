29ce5e50f8c0bac64f9e0ea6b8baf6692ad3cee4
mohamed emad <mohamedelbakrawy70@gmail.com>
2024-09-28 12:13:15 +0300

    create forget password cubit

---
+++ lib/features/auth/forget_password/logic/cubit/cubit/forgetpassword_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'forgetpassword_state.dart';
+
+class ForgetpasswordCubit extends Cubit<ForgetpasswordState> {
+  ForgetpasswordCubit() : super(ForgetpasswordInitial());
+}
