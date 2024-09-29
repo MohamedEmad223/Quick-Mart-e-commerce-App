5e63564f563352eccf0cc1c20804e7d66b6a6b6e
mohamed emad <mohamedelbakrawy70@gmail.com>
2024-09-29 16:12:56 +0300

    add relative path to app images

--- lib/core/helpers/app_images.dart
+++ lib/core/helpers/app_images.dart
@@ -12,4 +12,11 @@ class AppImages {
   static const String loginLogo = 'assets/svgs/on_boarding/quickmart.svg';
   //----------------success screen----------------
   static const String successLogo = 'assets/svgs/success/Rectangle 1741.svg';
+  //----------------home screen----------------
+  static const String homeLogo = 'assets/svgs/home/home-2.svg';
+  static const String categoriesLogo = 'assets/svgs/home/category-2.svg';
+  static const String cartLogo = 'assets/svgs/home/shopping-cart.svg';
+  static const String profileLogo = 'assets/svgs/home/profile.svg';
+  static const String wishlistLogo = 'assets/svgs/home/heart.svg';
+
 }
