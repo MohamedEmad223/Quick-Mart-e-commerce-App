055882b0961bc2b760aaa04f857acb2a1bf700e2
mohamed emad <mohamedelbakrawy70@gmail.com>
2024-09-29 18:35:36 +0300

    finish grid view and favourite icon

--- lib/features/home/views/screens/home_screen.dart
+++ lib/features/home/views/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../widgets/lable_row.dart';
+import '../widgets/product_gride_item.dart';
 import '../widgets/row_of_home_screen_widgets.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -20,9 +21,24 @@ class HomeScreen extends StatelessWidget {
               Image.asset('assets/images/Rectangle 6.png'),
               SizedBox(height: 20.h),
               const LableRow(
-                lable: 'Categories',
+                lable: 'Latest Products',
                 seeAll: 'See all',
-              )
+              ),
+              SizedBox(height: 20.h),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 5,
+                    mainAxisSpacing: 2,
+                    childAspectRatio: 1,
+                  ),
+                  itemCount: 6,
+                  itemBuilder: (context, index) {
+                    return const ProductGridItem();
+                  },
+                ),
+              ),
             ],
           ),
         ),
