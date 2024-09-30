9f66868156b54dff8fef138ed7d082182cfbafdc
mohamed emad <mohamedelbakrawy70@gmail.com>
2024-09-30 16:39:47 +0300

    - add term and conditon

---
+++ lib/features/settings/views/screens/term_and_conditions.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TermAndConditions extends StatelessWidget {
+  const TermAndConditions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Column(
+        children: [
+          Text('Term and Conditions'),
+        ],
+      ),
+    );
+  }
+}
