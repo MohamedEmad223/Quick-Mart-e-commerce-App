82cc3df27c9583e57b398d86c2afa96eac492ebf
mohamed emad <mohamedelbakrawy70@gmail.com>
2024-10-03 20:21:27 +0300

    add details screen

---
+++ lib/features/home/views/screens/details_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/theming/app_colors.dart';
+import '../../../../core/theming/app_text_style.dart';
+import '../../../../core/widgets/custom_button.dart';
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen(
+      {super.key,
+      required this.image,
+      required this.price,
+      required this.productName});
+  final String image;
+  final String price;
+  final String productName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Image.asset(image),
+        Text(productName),
+        Text(price),
+        Row(
+          children: [
+            CustomButton(
+                buttonText: 'Log In',
+                buttonAction: () {},
+                buttonStyle: AppTextStyle.plusJakartaSans16BoldWhite,
+                color: AppColors.blackColor,
+                height: 50.h)
+          ],
+        )
+      ],
+    ));
+  }
+}
